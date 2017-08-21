18a104d76dc528d396d0f78ff4c61ac1e2f79cdd
TPS <letsgotps@gmail.com>
2017-08-22 00:05:35 +0800

    lc310

--- LeetCode/lc310.cpp
+++ LeetCode/lc310.cpp
@@ -5,8 +5,73 @@
 
 using namespace std;
 
-class Solution {
+class Solution310 {
 public:
+
+	vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+		if(n < 3){
+            vector<int> t;
+            if(n >=2)
+                t.push_back(1);
+            if(n >= 1)
+                t.push_back(0);
+            return t;
+            
+        }
+        
+        vector<int> cnt(n, 0);
+		unordered_multimap<int, int> g;
+		for (int i = 0; i < edges.size(); i++)
+		{
+			g.insert({ edges[i].first, edges[i].second });
+			g.insert({ edges[i].second, edges[i].first });
+			cnt[edges[i].first]++;
+			cnt[edges[i].second]++;
+		}
+
+		vector<int> v1, v2, *now, *next, *temp;
+		now = &v1;
+		next = &v2;
+		int total = n;
+		for(int i = 0; i< n; i++){
+			if(cnt[i] == 1){
+				now->push_back(i);
+                cnt[i] = 0;
+                total--;
+			}
+		}
+		while(total > 2){
+			for(int i = 0; i < now->size(); i++){
+				auto range = g.equal_range((*now)[i]);
+                
+				for(auto itr = range.first; itr != range.second; itr++){
+					int x = itr->second;
+					if(cnt[x] > 0){
+						cnt[x]--;
+					}
+				}
+			}
+			for(int i = 0; i< n; i++){
+				if(cnt[i] == 1){
+					next->push_back(i);
+                    cnt[i] = 0;
+                    total--;
+				}
+			}
+			temp = now; now = next; next = temp; next->clear();
+
+		}
+        vector<int> re;
+        for(int i = 0; i< n; i++){
+            if(cnt[i] > 0)
+                re.push_back(i);
+        }		
+        return re;
+		
+	}
+
+
+#if 0
 	int maxg;
 
 	void dfs(unordered_map<int, int>& g, int now, vector<bool>& flag, vector<int>& cnt){
@@ -38,4 +103,5 @@ public:
 		maxg = 0;
 		dfs(g, 0,)
 	}
+#endif
 };
