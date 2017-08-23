a33fdfd0f6fa50d18a42e08076c9bd25d4f13e13
Captain <none>
2017-08-23 15:01:57 +0800

    lc321

--- LeetCode/lc321.cpp
+++ LeetCode/lc321.cpp
@@ -1,10 +1,23 @@
 #include <vector>
+#include <list>
 
 using namespace std;
 
 class Solution {
 public:
 	vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+		list<int> seq1(nums1.begin(), nums1.end());
+		list<int> seq2(nums2.begin(), nums2.end());
 
+		vector<list<int>> alls1(nums1.size() + 1), alls2(nums2.size() + 1);
+		alls1.back() = seq1;
+		alls2.back() = seq2;
+		for (int i = alls1.size() - 2; i > 0; i--){
+			alls1[i] = alls1[i + 1];
+			for (auto itr = alls1[i].begin(); itr != alls1[i].end();){
+				auto ti = itr;
+
+			}
+		}
 	}
 };
