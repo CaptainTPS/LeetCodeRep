8c1fdb31613173369f3ec7ca4a421a845481072b
Captain <none>
2017-10-18 16:53:39 +0800

    lc380

--- LeetCode/lc380.cpp
+++ LeetCode/lc380.cpp
@@ -8,18 +8,39 @@ public:
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        
+        auto finder = sdata.find(val);
+        if(finder != sdata.end())
+        	return false;
+
+        int n = vdata.size();
+        sdata[val] = n;
+        vdata.push_back(val);
+        return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        
+        if(sdata.find(val) == sdata.end())
+        	return false;
+
+        int v = vdata.back();
+        int pos = sdata[val];
+        sdata[v] = pos;
+        vdata[pos] = v;
+        vdata.pop_back();
+        sdata.erase(val);
+        return true;
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-        
+        int pos = rand() % vdata.size();
+        return vdata[pos];
     }
+
+private:
+	vector<int> vdata;
+	unordered_map<int, int> sdata;
 };
 
 /**
